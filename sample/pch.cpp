6a04e21599d101654cb8ef6294f3f1c5938a712c
Park Dong Ha[ 박동하 ] <dong-ha-park@linecorp.com>
2025-01-17 17:03:17 +0900

    create/build visual studio project

---
+++ sample/pch.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+
+#include <RestrictedErrorInfo.h>
+#include <hstring.h>
---
+++ sample/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <Windows.h>
+#undef GetCurrentTime
+#undef GetObject
+
+#include <Unknwn.h>
+
+#include <winrt/microsoft.ui.composition.h>
+#include <winrt/microsoft.ui.dispatching.h>
+#include <winrt/microsoft.ui.xaml.controls.h>
+#include <winrt/microsoft.ui.xaml.controls.primitives.h>
+#include <winrt/microsoft.ui.xaml.data.h>
+#include <winrt/microsoft.ui.xaml.h>
+#include <winrt/microsoft.ui.xaml.interop.h>
+#include <winrt/microsoft.ui.xaml.markup.h>
+#include <winrt/microsoft.ui.xaml.media.h>
+#include <winrt/microsoft.ui.xaml.navigation.h>
+#include <winrt/microsoft.ui.xaml.shapes.h>
+#include <winrt/windows.applicationmodel.activation.h>
+#include <winrt/windows.foundation.collections.h>
+#include <winrt/windows.foundation.h>
